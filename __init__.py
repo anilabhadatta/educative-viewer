660bd557821949e31800261e7c923210c8987fe2
Anilabha Datta <anilabhadatta@gmail.com>
2024-03-22 02:15:57 +0530

    updated url path for static

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ ROOT_DIR = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/edu-viewer/static')
 
     app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
